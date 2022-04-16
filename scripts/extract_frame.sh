7da1573206fc0a4a489b022296a2615268b1c4b4
VictoriaHY <50571797+VictoriaHY@users.noreply.github.com>
2022-04-16 17:49:48 -0400

    scripts

---
+++ scripts/extract_frame.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ ! -d "/data/ego/video_imgs"  ];then
+	mkdir "/data/ego/video_imgs"
+fi
+for file in `ls /data/ego/videos/*`
+do
+	name=$(basename $file .mp4)
+	echo "$name"
+	PTHH=/data/ego/video_imgs/$name
+	if [ ! -d "$PTHH"  ];then
+		mkdir "$PTHH"
+	fi
+	ffmpeg -i "$file" -f image2 -vf fps=30 -qscale:v 2 "$PTHH/img_%05d.jpg"
+done
---
+++ scripts/extract_wave.sh
@@ -0,0 +1,9 @@
+if [ ! -d "/data/ego/wave"  ];then
+	mkdir "/data/ego/wave"
+fi
+for f in `ls /data/ego/videos/`
+do
+    echo ${f%.*}
+    ffmpeg -y -i /data/ego/videos/${f} -qscale:a 0 -ac 1 -vn -threads 6 -ar 16000 /data/ego/wave/${f%.*}.wav -loglevel panic
+done
+
